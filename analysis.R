c53486dcfb4c669f007466d225336506d97beeb4
mikk8718 <baunsgaard38@gmail.com>
2025-05-21 18:15:37 +0200

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -5,3 +5,8 @@ library(jsonlite)
 dataset <- read.csv("~/Desktop/FootballData/Data/PL_23:24.csv")
 filteredDataset <- dataset %>% select_if(~ !any(is.na(.)))
 filteredDataset$events <- lapply(filteredDataset$events, fromJSON)
+
+ggplot(filteredDataset, aes(fill=as.factor(result), y=Offsides)) + 
+  geom_boxplot() + facet_wrap(~ as.factor(location))
+
+summary(filteredDataset[, 5:12])
